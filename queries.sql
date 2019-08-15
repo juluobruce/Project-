320d1d8263fa9b2e15f30c49a619e8a55a1d619d
juluobruce <42151923+juluobruce@users.noreply.github.com>
2019-08-14 17:35:40 -0700

    update

---
+++ queries.sql
@@ -0,0 +1,44 @@
+DROP TABLE education
+
+CREATE TABLE education (
+	"Year" INT NOT NULL,
+	"State" VARCHAR(50) NOT NULL,
+	"District Name" VARCHAR(50) NOT NULL, 
+	"Average Score" FLOAT NOT NULL,
+	"Test Subject" VARCHAR(225) NOT NULL,
+	"Grade Year" INT NOT NULL,
+	"Total Revenue" FLOAT NOT NULL,
+	"Instructors Expenditures" FLOAT NOT NULL,
+	"Support Services Expenditures" FLOAT NOT NULL,
+	"Other Expenditures" FLOAT
+);
+
+
+SELECT * FROM education
+
+
+SELECT
+"State",
+"Average Score",
+"Total Revenue"
+from education
+
+Select 
+"State",
+"Test Subject",
+"Total Revenue",
+MAX("Average Score") as "Max Avg Score",
+Min("Average Score") as "Min Avg Score"
+from education 
+Group by "Total Revenue","State", "Test Subject"
+Order by "Total Revenue" DESC;
+
+
+Select 
+"State",
+--"Test Subject",
+max("Average Score") as Score
+from education
+GROUP BY "State" --"Test Subject"
+Order by Score DESC;
+
