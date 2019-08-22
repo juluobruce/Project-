4dbd5af494fe2e8f9946d3ee3ed265f8a99179f0
juluobruce <42151923+juluobruce@users.noreply.github.com>
2019-08-21 22:18:36 -0700

    update

--- edfinance.sql
+++ edfinance.sql
@@ -15,10 +15,31 @@ CREATE TABLE states (
 	sepnding_supprtsrvcs FLOAT,
 	other_curr_spending FLOAT,
 	capital_outlay_exp FLOAT,
-	PRIMARY KEY (state, enroll, year, total_rev, fed_rev, state_rev, local_rev, total_exp,
-				spending_instruction, other_curr_spending, capital_outlay_exp)
+	PRIMARY KEY (state, enroll, year)
 );
 
 SELECT * from states;
 
 -- uploading districts 
+
+DROP TABLE districts
+
+CREATE TABLE districts (
+	state VARCHAR(225) NOT NULL,
+	enroll FLOAT NULL,
+	district VARCHAR(225,)
+	year FLOAT,
+	total_rev FLOAT,
+	fed_rev FLOAT,
+	state_rev FLOAT,
+	local_rev FLOAT,
+	total_exp FLOAT,
+	spending_instruction FLOAT,
+	sepnding_supprtsrvcs FLOAT,
+	other_curr_spending FLOAT,
+	capital_outlay_exp FLOAT,
+	FOREIGN KEY (state) REFERENCES states (state),
+	FOREIGN KEY () REFERENCES states ()
+	
+
+);
