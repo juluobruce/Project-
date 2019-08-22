b275cce5a2d8f898f49289f9f0d28833186f8566
juluobruce <42151923+juluobruce@users.noreply.github.com>
2019-08-21 18:12:10 -0700

    update

---
+++ edfinance.sql
@@ -0,0 +1,15 @@
+DROP TABLE states
+
+CREATE TABLE states (
+	state VARCHAR(225) NOT NULL,
+	year FLOAT NOT NULL,
+	enroll FLOAT,
+	total_rev FLOAT,
+	state_rev FLOAT,
+	local_rev FLOAT,
+	total_exp_ FLOAT,
+	spending_instruction FLOAT,
+	sepnding_supprtsrvcs FLOAT,
+	other_curr_spending FLOAT,
+	capital_outlay_exp FLOAT
+);
